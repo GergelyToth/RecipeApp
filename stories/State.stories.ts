817264bd034f6c1f8cd050ed40d3d88ac4a6461d
Gergely Toth <tgergely18@gmail.com>
2024-11-03 10:21:58 +0000

    feat: ui/state

---
+++ stories/State.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { State } from '#app/components/ui/state.tsx';
+
+const meta = {
+  title: 'ui/State',
+  component: State,
+} satisfies Meta<typeof State>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Active: Story = {
+  args: {
+    name: 'Tags',
+    active: true,
+  },
+}
+
+export const InActive: Story = {
+  args: {
+    name: 'Tags',
+    active: false,
+  },
+}
