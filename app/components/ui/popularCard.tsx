cde53d63e6cc524bd62e850cc5e65854f3e13619
Gergely Toth <tgergely18@gmail.com>
2024-11-04 18:23:45 +0000

    feat: ui/popularCard

---
+++ app/components/ui/popularCard.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+import { cn } from '#app/utils/misc.tsx';
+
+import { Rate } from '#app/components/ui/rate';
+import { Icon, IconName } from '#app/components/ui/icon';
+
+interface PopularCardProps extends React.HTMLAttributes<HTMLDivElement>{
+  name: string;
+  link: string;
+  imageUrl: string;
+  rating: number;
+  iconName?: IconName;
+}
+
+const PopularCard = React.forwardRef<HTMLDivElement, PopularCardProps>(({ name, link, imageUrl, rating, iconName, className, ...props }, ref) => {
+  return (
+    <div className={cn('relative rounded flex flex-col justify-between w-40 h-52 p-2 overflow-hidden', className)} ref={ref} {...props}>
+      <div className='absolute -top-2 left-0 -z-10 w-full h-1/2 bg-gradient-to-b from-base-black to-transparent' />
+      <div className='absolute -bottom-2 left-0 -z-10 w-full h-1/2 bg-gradient-to-t from-base-black to-transparent' />
+
+      <div className='flex flex-row justify-between'>
+        <Rate rating={rating} />
+
+        {/* TODO: icon click & hover */}
+        {iconName && <Icon name={iconName} className='w-6 h-6 text-base-white' />}
+      </div>
+
+      <img
+        className="-z-20 absolute inset-0 w-full h-full object-cover"
+        src={imageUrl}
+        alt={name}
+      />
+      <a className='font-primary font-bold text-base-white' href={link}>{name}</a>
+    </div>
+  );
+});
+PopularCard.displayName = 'PopularCard';
+
+export { PopularCard };
---
+++ stories/PopularCard.stories.ts
@@ -0,0 +1,21 @@
+import  { type Meta, type StoryObj } from '@storybook/react';
+
+import { PopularCard } from '#app/components/ui/popularCard';
+
+const meta = {
+  title: 'ui/PopularCard',
+  component: PopularCard,
+} satisfies Meta<typeof PopularCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    imageUrl: 'https://picsum.photos/300/300',
+    link: '#',
+    name: 'chocolate cake with buttercream frosting',
+    rating: 4.8,
+    iconName: 'outline/archive',
+  },
+}
